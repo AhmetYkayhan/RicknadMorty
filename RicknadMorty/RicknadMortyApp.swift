a169c84e8601bb7932b6237e610499fc971a16b2
AhmetYKayhan <kayhanyasinahmet@gmail.com>
2026-04-27 20:58:09 +0300

    Initial Commit

---
+++ RicknadMorty/RicknadMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicknadMortyApp.swift
+//  RicknadMorty
+//
+//  Created by Yasin Kayhan on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct RicknadMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
